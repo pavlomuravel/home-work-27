fa52645b699259538ca36b5bf0df0a22a092e1a5
Pavlo Muravel <luis.proskuriv@gmail.com>
2025-02-28 21:11:18 +0100

    add newJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,23 +4,88 @@ const imgList = [
   'img_1.jpg',
   'img_2.jpg',
   'img_3.jpg',
-  'img_4.jpg',
+  'img_4.jpg'
 ];
 
-let position = 0;
-const leftNextElement = document.querySelector("#leftNext");
-const rightNextElement = document.querySelector("#rightNext");
-const imgContainer = document.querySelector("#imgContainer img");
+let imgSlaid = 0;
+const slaidCount = imgList.length;
+let imageWidth;
+let isSlaiding = false;
+let slidingIntervalId;
+const slideTime = 1;
 
-function leftNextClick() {
-  position += 100;
-  imgContainer.style.transform = `translateX(${position}%)`;
+generateImg();
+generateDots();
+
+
+const leftBack = document.querySelector('#leftBack');
+const rightNext = document.querySelector('#rightNext');
+const imgConteiner = document.querySelector('#imgContainer');
+const firstImg = document.querySelector('#imgContainer img');
+const currentbutton = document.querySelector('#current__button');
+
+
+imageWidth = firstImg.offsetWidth;
+
+leftBack.addEventListener('click', onLeftClick);
+rightNext.addEventListener('click', onRightClick);
+currentbutton.addEventListener('click', onDotClick);
+
+function generateImg() {
+  let resultHtml = '';
+  imgList.forEach(imgList => {
+    resultHtml += `<img src="assets/images/${imgList}" alt="#">`;
+
+  })
+  document.querySelector('#carousel .carousel__container #imgContainer').innerHTML = resultHtml;
+  return resultHtml;
 };
-function rightNextClick() {
-  position -= 100;
-  imgContainer.style.transform = `translateX(${position}%)`;
+
+function generateDots() {
+  let resultHtml = '';
+  imgList.forEach((imgList, index) => {
+    const activeDot = index === 0 ? 'active' : '';
+    resultHtml += `<div class="current__button-item ${activeDot}" data-img="${index}"></div>`;
+  })
+
+  document.querySelector('#carousel #current__button').innerHTML = resultHtml;
 
 };
-leftNextElement.addEventListener("click", leftNextClick);
-rightNextElement.addEventListener("click", rightNextClick);
 
+function onDotClick() {
+  let resultHtml = '';
+  imgList.forEach((imgList, index) => {
+    const classActive = index === 0 ? 'active' : '';
+    resultHtml += `<div class="current__button-item ${classActive}" data-img="${index}"></div>`;
+  })
+  document.querySelector('#carousel #current__button').innerHTML = resultHtml;
+};
+
+function onLeftClick() {
+  imgSlaid--;
+
+  if(imgSlaid < 0) {
+    imgSlaid = slaidCount - 1;
+  }
+
+  imgConteiner.style.transform = `translate(-${imgSlaid * firstImg.offsetWidth}px)`;
+  refreshActiveDot();
+};
+function onRightClick() {
+  imgSlaid++;
+
+  if(imgSlaid === slaidCount) {
+    imgSlaid = 0;
+  }
+
+  imgConteiner.style.transform = `translate(-${imgSlaid * firstImg.offsetWidth}px)`;
+  refreshActiveDot();
+};
+
+function refreshActiveDot() {
+  const activeDot = document.querySelector('#current__button .active');
+
+  if(activeDot) {
+    activeDot.classList.remove('active');
+  }
+}
